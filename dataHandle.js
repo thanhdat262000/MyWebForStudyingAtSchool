8016441298e22075c3f299660cd20aa47f2ab73d
thanhdat262000 <49014572+thanhdat262000@users.noreply.github.com>
2020-05-04 00:01:47 +0700

    add-port-musql

--- dataHandle.js
+++ dataHandle.js
@@ -3,7 +3,8 @@ const connection = mysql.createConnection({
 	host: process.env.HOST,
 	user: process.env.USER,
 	password: process.env.PW,
-	database: process.env.DB
+	database: process.env.DB,
+	port :'3306'
 
 })
 module.exports = connection;
