d87db299abbb6a4c9551253f1a8e1e9b811b401e
thanhdat262000 <49014572+thanhdat262000@users.noreply.github.com>
2020-05-03 22:41:53 +0700

    hide-db-info

--- dataHandle.js
+++ dataHandle.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 const connection = mysql.createConnection({
-	host: 'remotemysql.com',
-	user: 'Fxt7yonmU1',
-	password: '7uEywcuh7N',
-	database: 'Fxt7yonmU1'
+	host: HOST,
+	user: USER,
+	password: PS,
+	database: DB
 
 })
 module.exports = connection;
