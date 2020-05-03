ddb0eac8a905cac58f0ab8b9e24b6baa9eff87b4
thanhdat262000 <49014572+thanhdat262000@users.noreply.github.com>
2020-05-03 23:23:18 +0700

    adjust-env

--- dataHandle.js
+++ dataHandle.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 const connection = mysql.createConnection({
 	host: process.env.HOST,
 	user: process.env.USER,
-	password: process.env.PS,
+	password: process.env.PW,
 	database: process.env.DB
 
 })
