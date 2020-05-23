2970f92d475816840deb440044ce99c3c377b22b
thanhdat262000 <49014572+thanhdat262000@users.noreply.github.com>
2020-05-23 10:41:05 +0700

    update-Cart

--- controller/cart.controller.js
+++ controller/cart.controller.js
@@ -26,7 +26,7 @@ const connection = require('../dataHandle');
 
 module.exports.add = function(req, res) {
 	var productId = req.params.productId;
-	let sql = 'SELECT * FROM `products` WHERE productId = ?';
+	let sql = 'SELECT productId,`productName`, `productBrand`, `productScreen`, `productCPU`, `productRAM`, `productVGA`, `productOS`, `productWeight`, `productImage`, buyPrice, productImage FROM `products` WHERE productId = ?';
 	connection.query(sql, [productId], function(error,result,field) {
 		if (!req.session.countTotal) {
 			req.session.countTotal =1;
@@ -36,22 +36,48 @@ module.exports.add = function(req, res) {
 			req.session.countTotal++;
 			req.session.totalPrice += result[0].buyPrice;
 		}
-		console.log(typeof(req.session.totalPrice));
 		if (!req.session.cart) {
-			req.session.cart = {};
+			req.session.cart = [];
 			var product = result[0];
 			product.quantity = 1;
-			req.session.cart[productId]= product;
+			req.session.cart.push(product);
 		} else {
-			if (!req.session.cart[productId]) {
+			var newItem = true;
+			for (let p of req.session.cart) {
+				if (p.productId == result[0].productId) {
+					newItem = false
+					p.quantity++;
+				}
+			}
+			if (newItem) {
 				var product = result[0];
 				product.quantity = 1;
-				req.session.cart[productId]= product;
-			} else {
-			var quantity =req.session.cart[productId].quantity;
-			req.session.cart[productId].quantity = quantity + 1;
-		}
+				req.session.cart.push(product);
+			}
 		}
 		res.redirect('back');
 	})
+}
+module.exports.update = function(req, res) {
+	var quantity = req.body.quantity;
+	var productId = req.body.id;
+	var totalPrice= req.body.totalPrice;
+	var count = req.body.count;
+	for (var i =0;i < req.session.cart.length; i++) {
+		if (req.session.cart[i].productId == productId) {
+			if (quantity == 0) {
+				req.session.cart.splice(i,1);
+				req.session.totalPrice = totalPrice;
+				req.session.countTotal+= parseInt(count);
+			} else {
+			req.session.cart[i].quantity = quantity;
+			req.session.totalPrice = totalPrice;
+			req.session.countTotal+= parseInt(count);
+			}
+			break;
+		}
+	}
+		res.render('product/showCart', {
+			purchasedProducts: req.session.cart
+		})
 }
